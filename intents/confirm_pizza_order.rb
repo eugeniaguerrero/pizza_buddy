ac72265a992f96d9267f498e7b0d9a254c0ad5c9
Eugenia <angeleuge95@gmail.com>
2017-05-24 11:37:32 +0100

    Removes extra pound sign line 11

--- intents/confirm_pizza_order.rb
+++ intents/confirm_pizza_order.rb
@@ -8,7 +8,7 @@ intent "ConfirmPizzaOrder" do
   response_text = ["Thanks! Your #{ pizza.size } pizza with #{ pizza.toppings.to_sentence } is on ",
                    "its way to you. Your order ID is #{ pizza.id }. Thank you for using Pizza Buddy!"].join
   
-  card_title = "Your Pizza Order ##{ pizza.id }"
+  card_title = "Your Pizza Order #{ pizza.id }"
   card_body = "You ordered a #{ pizza.size } pizza with #{ pizza.toppings.to_sentence }!"
   card_image = "https://image.ibb.co/jeRZLv/alexa_pizza.png"
   pizza_card = card(card_title, card_body, card_image)
